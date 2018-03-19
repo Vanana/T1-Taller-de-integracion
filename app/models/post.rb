aa2036349eaff3a1b172024494ecff162f68581d
Vanana <vblasco@uc.cl>
2018-03-19 07:15:54 -0300

    init

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_many :comments, :dependent => :destroy
 
-  validates_length_of :titulo, :within => 2..50
+  validates_length_of :titulo, :within => 2..100
   validates_length_of :bajada, :within => 2..200
   validates_presence_of :body
 
